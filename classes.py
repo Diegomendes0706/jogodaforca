df8c9c890272819d0e2366d2c1b9f37e3ac60cc5
Diegomendes0706 <diego.mendesgarcia@gmail.com>
2023-06-09 14:35:41 -0300

    commit3

---
+++ classes.py
@@ -0,0 +1,7 @@
+class Pessoa:
+    def __init__(self,nome,cpf, email, login, senha):
+        self.nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.login = login
+        self.senha = senha
--- jogo.py
+++ jogo.py
@@ -1,10 +1,47 @@
+import sqlite3
+from classes import Pessoa
+banco = sqlite3.connect('jogo_forca.db')
+cursor = banco.cursor()
+adm = 'Administrador'
+jogador = 'Jogador'
+p = 'Perguntas'
+
+def criartabela(tipo):
+    if tipo == adm:
+        cursor.execute('CREATE TABLE Administrador(nome text, email text, login text, senha text)')
+    elif tipo == jogador :
+        cursor.execute('CREATE TABLE Jogador (cpf text, nome text, email text, login text, senha text) ')
+    elif tipo == p:
+        cursor.execute('CREATE TABLE Perguntas (pergunta text)')
+def adcionaraobanco(tipo,cpf,nome, email,login,senha):
+    if tipo == adm:
+        login = senha = 'admin'
+        cursor.execute(f"INSERT INTO {tipo} VALUES('{input('nome: ')}', {input('email: ')}', '{input('login: ')}', '{input('senha: ')}')")
+    elif tipo == jogador:
+        cursor.execute(f'INSERT INTO {tipo} VALUES("{cpf}","{nome}","{email}","{login}","{senha}")')
+    banco.commit()
+    print('adcionado com sucesso')
+
+def deletardobanco(tipo, cpf):
+    try:
+        cursor.execute(f'DELETE from {tipo} WHERE cpf = {cpf}')
+        banco.commit()
+        print('Excluido com sucesso')
+    except sqlite3.Error as erro:
+        print(f'Erro ao exluir: {erro}')
+def verbanco(tipo):
+    cursor.execute(f'SELECT * FROM {tipo}')
+    print(cursor.fetchall())
+
+
+verbanco('Administrador')
 while True:
     print(('-------------- Menu Principal --------------\n'
-                  '1 – Jogar\n'
-                  '2 – Cadastrar Novo Jogador\n'
-                  '3 – Recuperar Senha\n'
-                  '4 – Entrar como Administrador\n'
-                  '5 – Sair'))
+           '1 – Jogar\n'
+           '2 – Cadastrar Novo Jogador\n'
+           '3 – Recuperar Senha\n'
+           '4 – Entrar como Administrador\n'
+           '5 – Sair'))
     try:
         o = int(input('Digite sua opção: '))
         if o == 1:
@@ -23,4 +60,3 @@ while True:
             print('Opção invalida,digite uma das opcões:')
     except ValueError:
         print('Opção invalida, digite uma das opções: ')
-
